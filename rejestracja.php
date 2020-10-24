3808d42da3678df83192f46c7ec8c3e82a4b9e9b
ukasz1 <46789003+ukasz1@users.noreply.github.com>
2020-10-24 15:53:10 +0200

    validation of the registration form

--- rejestracja.php
+++ rejestracja.php
@@ -18,9 +18,38 @@
 		
 		$nick=$_POST['nick'];
 		
-		if((strlen($nick)<3) || (strlen($nick)>20)){	//sprawdzenie długości nicku
+		if(strlen($nick)<3 || strlen($nick)>20){	//sprawdzenie długości nicku
 			$wszystko_OK=false;
 			$_SESSION['e_nick']='Nick musi posiadać od 3 do 20 znaków!';
+		}
+		
+		if(ctype_alnum($nick)==false){				//ctype_alnum() - funkcja do sprawdzania znaków alfanumerycznych
+			$wszystko_OK=false;
+			$_SESSION['e_nick']="Nick może składać się tylko z liter i cyfr (bez polskich znaków)";
+		}
+		
+		//Sprawdzanie poprawności adresu e-mail
+		$email = $_POST['email'];
+		
+		$emailB = filter_var($email, FILTER_SANITIZE_EMAIL);	//filter_var($zmienna, FILTER_SANITIZE_EMAIL) - funkcja wycinająca z napisu niedopuszczalne znaki
+		
+		if(filter_var($email,FILTER_VALIDATE_EMAIL)!=true || $email!=$emailB){	//filter_var($zmienna,FILTER_VALIDATE_EMAIL) - funkcja boolowska sprawdzająca poprawność formy e-maila
+			$wszystko_OK=false;
+			$_SESSION['e_email']="Podaj poprawny adres e-mail";
+		}
+		
+		//Sprawdzanie poprawności hasła
+		$haslo1 = $_POST['haslo1'];
+		$haslo2 = $_POST['haslo2'];
+		
+		if(strlen($haslo1)<8 || strlen($haslo1)>20){
+			$wszystko_OK=false;
+			$_SESSION['e_haslo1']="Hasło musi posiadać od 8 do 20 znaków";
+		}
+		
+		if($haslo1!=$haslo2){
+			$wszystko_OK=false;
+			$_SESSION['e_haslo2']="Podane hasła nie są identyczne";
 		}
 		
 		
@@ -60,17 +89,39 @@
 		
 		<?php
 			if (isset($_SESSION['e_nick'])){
-				echo '<div class="error">'.$_SESSION['e_nick'].'</div>';
+				echo '<div class="error">'.$_SESSION['e_nick'].'</div>';	//Komentarz: nieprawidłowy nick
 				unset($_SESSION['e_nick']);
-				
 			}
 		?>
 		
 		E-mail:<br /><input type="text" name="email" /><br />
 		
+		<?php
+			if (isset($_SESSION['e_email'])){
+				echo '<div class="error">'.$_SESSION['e_email'].'</div>';	//Komunikat: nieprawidłowy e-mail
+				unset($_SESSION['e_email']);	
+			}
+		?>	
+		
 		Hasło: <br /><input type="password" name="haslo1" /><br />
 		
+		<?php
+			if (isset($_SESSION['e_haslo1'])){
+				echo '<div class="error">'.$_SESSION['e_haslo1'].'</div>';	//Komunikat: złą długość hasła
+				unset($_SESSION['e_haslo1']);								
+				
+			}
+		?>
+		
 		Powtórz hasło: <br /><input type="password" name="haslo2" /><br />
+		
+		<?php
+			if (isset($_SESSION['e_haslo2'])){
+				echo '<div class="error">'.$_SESSION['e_haslo2'].'</div>';	//Komunikat: różne hasła
+				unset($_SESSION['e_haslo2']);								
+				
+			}
+		?>
 		
 		<label>
 			<input type="checkbox" name="regulamin"/>Akceptuję regulamin
